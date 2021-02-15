5c8be0d1a3c0cb188a414f7e43b2d19021e84910
cs-lucas-costa <lucas.henrique.costa@concrete.com.br>
2021-02-15 13:35:26 -0300

    Create expansion model

---
+++ Bootcamp-Magic/Model/Expansion.swift
@@ -0,0 +1,13 @@
+//
+//  Expansion.swift
+//  Bootcamp-Magic
+//
+//  Created by lucas.henrique.costa on 15/02/21.
+//
+
+import Foundation
+
+struct Expansion: Decodable {
+    let code: String
+    let name: String
+}
