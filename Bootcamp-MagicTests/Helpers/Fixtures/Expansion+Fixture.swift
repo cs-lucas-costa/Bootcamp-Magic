b685edd2f1b790bf7a680142b0959b46ba768a36
cs-lucas-costa <lucas.henrique.costa@concrete.com.br>
2021-02-15 16:21:52 -0300

    Test Dict Expansio

--- Bootcamp-MagicTests/Helpers/Fixtures/Expansion+Fixture.swift
+++ Bootcamp-MagicTests/Helpers/Fixtures/Expansion+Fixture.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+@testable import Bootcamp_Magic
+
+extension Expansion {
+    
+    static func fixture(
+        name: String = "Tenth Edition",
+        code: String = "10E") -> Self {
+        
+        .init(code: code, name: name)
+    }
+    
+}
+
+extension ExpansionList {
+    
+    static func fixture() throws -> Self {
+        try JSONDecoder().decode(ExpansionList.self, from: data)
+    }
+    
+    private static var data: Data {
+        
+        Data("""
+        {
+            "sets": [
+                {
+                    "code": "10E",
+                    "name": "Tenth Edition",
+                },
+                {
+                    "code": "2ED",
+                    "name": "Unlimited Edition",
+                },
+                {
+                    "code": "2ED",
+                    "name": "Unlimited Edition",
+                }
+            ]
+        }
+        """.utf8)
+    }
+}
--- Bootcamp-MagicTests/ViewModel/Expansion/ExpansionListViewModelTestCase.swift
+++ Bootcamp-MagicTests/ViewModel/Expansion/ExpansionListViewModelTestCase.swift
@@ -6,27 +6,18 @@
 //
 
 import XCTest
+@testable import Bootcamp_Magic
 
 class ExpansionListViewModelTestCase: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testCreateDictExpansion() throws {
+        
+        let list = try ExpansionList.fixture()
+        let sut = ExpansionListViewModel(list: list)
+        
+        XCTAssertEqual(sut.dictExpansions.keys.count, 2)
+        XCTAssertEqual(sut.dictExpansions["U"]?.count, 2)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
