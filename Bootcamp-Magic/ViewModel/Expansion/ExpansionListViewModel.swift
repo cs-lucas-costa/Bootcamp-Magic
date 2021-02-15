6259952f819a4a077bab4d8a420bd624d756f313
cs-lucas-costa <lucas.henrique.costa@concrete.com.br>
2021-02-15 16:21:03 -0300

    Create new function to define dict expansion

--- Bootcamp-Magic/ViewModel/Expansion/ExpansionListViewModel.swift
+++ Bootcamp-Magic/ViewModel/Expansion/ExpansionListViewModel.swift
@@ -9,14 +9,27 @@ import Foundation
 
 class ExpansionListViewModel {
     
-    private let list: ExpansionList
+    private(set) var dictExpansions: [String: [ExpansionViewModel]] = [:]
     
     init(list: ExpansionList) {
-        self.list = list
+        createDictExpansions(from: list)
     }
     
-    var expansions: [ExpansionViewModel] {
-        list.expansions.map { ExpansionViewModel(expansion: $0) }
+    //MARK: Methods
+    private func createDictExpansions(from list: ExpansionList) {
+        list.expansions
+            .sorted(by: { $0.name < $1.name })
+            .forEach { [weak self] (expansion) in
+                
+                guard let self = self else { return }
+                guard let key = expansion.name.first?.uppercased() else { return }
+                
+                if !self.dictExpansions.keys.contains(key) {
+                    self.dictExpansions[key] = []
+                }
+                
+                self.dictExpansions[key]?.append(ExpansionViewModel(expansion: expansion))
+            }
     }
     
 }
