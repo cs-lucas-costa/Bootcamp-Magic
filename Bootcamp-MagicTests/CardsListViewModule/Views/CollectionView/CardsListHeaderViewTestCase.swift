7a171abf516a4395bce7ece64791ae2f0e65767c
cs-lucas-costa <lucas.henrique.costa@concrete.com.br>
2021-02-18 14:45:37 -0300

    Snapshot Tests to CardsListView

---
+++ Bootcamp-MagicTests/CardsListViewModule/Views/CollectionView/CardsListHeaderViewTestCase.swift
@@ -0,0 +1,36 @@
+//
+//  CardsListHeaderViewTestCase.swift
+//  Bootcamp-MagicTests
+//
+//  Created by lucas.henrique.costa on 18/02/21.
+//
+
+import XCTest
+import SnapshotTesting
+
+@testable import Bootcamp_Magic
+
+class CardsListHeaderViewTestCase: XCTestCase {
+
+    var sut: CardsListHeaderView!
+    
+    override func setUp() {
+        super.setUp()
+        let frame = CGRect(origin: .zero,
+                           size: CGSize(width: UIScreen.main.bounds.width, height: 40))
+        sut = CardsListHeaderView(frame: frame)
+//        isRecording = true
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func testHeaderView() {
+        sut.title = "Creature"
+        sut.setupView()
+        assertSnapshot(matching: sut, as: .image)
+    }
+    
+}
