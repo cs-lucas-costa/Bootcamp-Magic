c0ad43725d7c65a7f424c89ecf36f5fb0e367a4d
cs-lucas-costa <lucas.henrique.costa@concrete.com.br>
2021-02-19 18:45:00 -0300

    Refact Coordinators

--- Bootcamp-Magic/Coordinator/CardsListCoordinator.swift
+++ Bootcamp-Magic/Coordinator/CardsListCoordinator.swift
@@ -9,49 +9,5 @@ import UIKit
 
 protocol CardsListCoordinatorProtocol: AnyObject {
     func showCardDetail(_ cards: [CardViewModel])
+    func dismiss()
 }
-
-//final class CardsListCoordinator: Coordinatable {
-//
-//    private let state: CardsListViewState
-//    var currentViewController: UIViewController?
-//    var navigationController: UINavigationController
-//
-//    init(navigationController: UINavigationController,
-//         state: CardsListViewState) {
-//        self.navigationController = navigationController
-//        self.state = state
-//    }
-//
-//    func start() {
-//        let viewController = startViewController()
-//        viewController.coordinator = self
-//        currentViewController = viewController
-//        navigationController.pushViewController(viewController, animated: true)
-//    }
-//}
-//
-//// MARK: Private methods
-//private extension CardsListCoordinator {
-//    func startViewController() -> CardsListViewControllerProtocol {
-//        switch state {
-//        case .all(let expansion):
-//            return AllCardsListViewController(numberOfCardsPerRow: 3,
-//                                              viewModel: CardListViewModel(),
-//                                              with: expansion)
-//        case .favourites:
-//            return FavouritesCardsListViewController(numberOfCardsPerRow: 3,
-//                                                    viewModel: CardListViewModel())
-//        }
-//    }
-//}
-//
-//// MARK: Delegates
-//extension CardsListCoordinator: CardsListCoordinatorProtocol {
-//
-//    func showCardDetail(_ card: CardViewModel) {
-//        // TODOs: Realizar chamada coordinator de detalhes
-//
-//    }
-//
-//}
